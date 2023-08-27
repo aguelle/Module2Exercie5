7b5b91cee1122c735a4b7a3f270f83fbef8b7e72
Aurélien Guelle <aurelien.guelle@gmail.com>
2023-08-27 22:22:41 +0200

    Module 2, Exercice 5, Requestion 1 to 5

--- assets/learn.tribu-creative.io_pluginfile.php_6525_mod_resource_content_3_mod02-e5 - Séries.js
+++ assets/learn.tribu-creative.io_pluginfile.php_6525_mod_resource_content_3_mod02-e5 - Séries.js
@@ -1,186 +1,185 @@
 const series = {
-    "Arcane": {
-        releaseYear: 2021,
-        country: "U.S.A.",
-        availableOn: "Netflix",
-        styles: ["Aventure", "Science fiction", "Action", "Animation"],
-        createdBy: ["Alex Yee", "Christian \"Praeco\" Linke"],
-        actors: ["Hailee Steinfeld", "Ella Purnell", "Kevin Alejandro"],
-        episodeDurationInMinutes: 45,
-        numberOfSeasons: 2,
-        numberOfEpisods: 18,
-        ongoing: true
-    },
-    "Game of Thrones": {
-        releaseYear: 2011,
-        country: "U.S.A.",
-        availableOn: "OCS",
-        styles: ["Drame", "Fantastique"],
-        createdBy: ["D.B. Weiss", "David Benioff"],
-        actors: ["Emilia Clarke", "Peter Dinklage", "Kit Harington", "Lena Headey"],
-        episodeDurationInMinutes: 52,
-        numberOfSeasons: 8,
-        numberOfEpisods: 73,
-        ongoing: false
-    },
-    "Breaking Bad": {
-        releaseYear: 2008,
-        country: "U.S.A.",
-        availableOn: "Netflix",
-        styles: ["Drame"],
-        createdBy: ["Vince Gilligan"],
-        actors: ["Bryan Cranston", "Aaron Paul", "Anna Gunn", "Dean Norris"],
-        episodeDurationInMinutes: 42,
-        numberOfSeasons: 5,
-        numberOfEpisods: 62,
-        ongoing: false
-    },
-    "Chernobyl": {
-        releaseYear: 2019,
-        country: "U.S.A.",
-        availableOn: "OCS",
-        styles: ["Drame"],
-        createdBy: ["Craig Mazin"],
-        actors: ["Jared Harris", "Stellan Skarsgård", "Paul Ritter"],
-        episodeDurationInMinutes: 58,
-        numberOfSeasons: 1,
-        numberOfEpisods: 5,
-        ongoing: false
-    },
-    "Peaky Blinders": {
-        releaseYear: 2013,
-        country: "Grande-Bretagne",
-        availableOn: "Netflix",
-        styles: ["Drame", "Historique", "Policier"],
-        createdBy: ["Steven Knight"],
-        actors: ["Cillian Murphy", "Natasha O'Keeffe", "Helen McCrory"],
-        episodeDurationInMinutes: 52,
-        numberOfSeasons: 6,
-        numberOfEpisods: 36,
-        ongoing: false
-    },
-    "Shingeki no Kyojin": {
-        releaseYear: 2013,
-        country: "Japon",
-        availableOn: "Netflix",
-        styles: ["Aventure", "Action", "Animation"],
-        createdBy: ["Yasuko Kobayashi"],
-        actors: ["Yûki Kaji", "Yui Ishikawa", "Marina Inoue"],
-        episodeDurationInMinutes: 24,
-        numberOfSeasons: 4,
-        numberOfEpisods: 95,
-        ongoing: true
-    },
-    "Sherlock": {
-        releaseYear: 2010,
-        country: "Grande-Bretagne",
-        availableOn: "Amazon",
-        styles: ["Aventure", "Drame", "Policier"],
-        createdBy: ["Steven Moffat", "Mark Gatiss"],
-        actors: ["Benedict Cumberbatch", "Martin Freeman", "Amanda Abbington"],
-        episodeDurationInMinutes: 90,
-        numberOfSeasons: 4,
-        numberOfEpisods: 14,
-        ongoing: false
-    },
-    "The Queen's Gambit": {
-        releaseYear: 2020,
-        country: "U.S.A.",
-        availableOn: "Netflix",
-        styles: ["Drame"],
-        createdBy: ["Scott Frank", "Allan Scott"],
-        actors: ["Anya Taylor-Joy", "William Horberg", "Harry Melling"],
-        episodeDurationInMinutes: 60,
-        numberOfSeasons: 1,
-        numberOfEpisods: 7,
-        ongoing: false
-    },
-    "When They See Us": {
-        releaseYear: 2019,
-        country: "U.S.A.",
-        availableOn: "Netflix",
-        styles: ["Drame"],
-        createdBy: ["Ava DuVernay"],
-        actors: ["Asante Blackk", "Caleel Harris", "Ethan Herisse"],
-        episodeDurationInMinutes: 35,
-        numberOfSeasons: 1,
-        numberOfEpisods: 4,
-        ongoing: false
-    },
-    "Stranger Things": {
-        releaseYear: 2016,
-        country: "U.S.A.",
-        availableOn: "Netflix",
-        styles: ["Drame", "Fantastique", "Thriller"],
-        createdBy: ["Matt Duffer, Ross Duffer"],
-        actors: ["Winona Ryder", "Millie Bobby Brown", "Finn Wolfhard"],
-        episodeDurationInMinutes: 55,
-        numberOfSeasons: 5,
-        numberOfEpisods: 43,
-        ongoing: true
-    },
-    "Hunter X Hunter": {
-        releaseYear: 2011,
-        country: "Japon",
-        availableOn: "Netflix",
-        styles: ["Aventure", "Comédie", "Animation"],
-        createdBy: ["Megumi Han", "Keiji Fujiwara", "Miyuki Sawashiro"],
-        actors: ["Megumi Han", "Keiji Fujiwara", "Miyuki Sawashiro", "Mariya Ise"],
-        episodeDurationInMinutes: 22,
-        numberOfSeasons: 4,
-        numberOfEpisods: 148,
-        ongoing: false
-    },
-    "Heartstopper": {
-        releaseYear: 2022,
-        country: "Grande-Bretagne",
-        availableOn: "Netflix",
-        styles: ["Drame", "Romance"],
-        createdBy: ["Alice Oseman"],
-        actors: ["Kit Connor", "Joe Locke", "Corinna Brown"],
-        episodeDurationInMinutes: 30,
-        numberOfSeasons: 3,
-        numberOfEpisods: 16,
-        ongoing: true
-    },
-    "Vikings": {
-        releaseYear: 2013,
-        country: "Canada",
-        availableOn: "Netflix",
-        styles: ["Aventure", "Drame", "Historique"],
-        createdBy: ["Michael Hirst"],
-        actors: ["Alex Høgh Andersen", "Gustaf Skarsgård", "Marco Ilsø"],
-        episodeDurationInMinutes: 52,
-        numberOfSeasons: 6,
-        numberOfEpisods: 89,
-        ongoing: false
-    },
-    "True Detective": {
-        releaseYear: 2014,
-        country: "U.S.A.",
-        availableOn: "OCS",
-        styles: ["Drame", "Policier", "Thriller"],
-        createdBy: ["Nic Pizzolatto"],
-        actors: ["Mahershala Ali", "Stephen Dorff", "Carmen Ejogo"],
-        episodeDurationInMinutes: 55,
-        numberOfSeasons: 4,
-        numberOfEpisods: 24,
-        ongoing: true
-    },
-    "Shameless": {
-        releaseYear: 2011,
-        country: "U.S.A.",
-        availableOn: "Amazon",
-        styles: ["Comédie", "Drame"],
-        createdBy: ["Paul Abbott"],
-        actors: ["William H. Macy", "Jeremy Allen White", "Shanola Hampton"],
-        episodeDurationInMinutes: 45,
-        numberOfSeasons: 11,
-        numberOfEpisods: 134,
-        ongoing: false
-    }
+  Arcane: {
+    releaseYear: 2021,
+    country: "U.S.A.",
+    availableOn: "Netflix",
+    styles: ["Aventure", "Science fiction", "Action", "Animation"],
+    createdBy: ["Alex Yee", 'Christian "Praeco" Linke'],
+    actors: ["Hailee Steinfeld", "Ella Purnell", "Kevin Alejandro"],
+    episodeDurationInMinutes: 45,
+    numberOfSeasons: 2,
+    numberOfEpisods: 18,
+    ongoing: true,
+  },
+  "Game of Thrones": {
+    releaseYear: 2011,
+    country: "U.S.A.",
+    availableOn: "OCS",
+    styles: ["Drame", "Fantastique"],
+    createdBy: ["D.B. Weiss", "David Benioff"],
+    actors: ["Emilia Clarke", "Peter Dinklage", "Kit Harington", "Lena Headey"],
+    episodeDurationInMinutes: 52,
+    numberOfSeasons: 8,
+    numberOfEpisods: 73,
+    ongoing: false,
+  },
+  "Breaking Bad": {
+    releaseYear: 2008,
+    country: "U.S.A.",
+    availableOn: "Netflix",
+    styles: ["Drame"],
+    createdBy: ["Vince Gilligan"],
+    actors: ["Bryan Cranston", "Aaron Paul", "Anna Gunn", "Dean Norris"],
+    episodeDurationInMinutes: 42,
+    numberOfSeasons: 5,
+    numberOfEpisods: 62,
+    ongoing: false,
+  },
+  Chernobyl: {
+    releaseYear: 2019,
+    country: "U.S.A.",
+    availableOn: "OCS",
+    styles: ["Drame"],
+    createdBy: ["Craig Mazin"],
+    actors: ["Jared Harris", "Stellan Skarsgård", "Paul Ritter"],
+    episodeDurationInMinutes: 58,
+    numberOfSeasons: 1,
+    numberOfEpisods: 5,
+    ongoing: false,
+  },
+  "Peaky Blinders": {
+    releaseYear: 2013,
+    country: "Grande-Bretagne",
+    availableOn: "Netflix",
+    styles: ["Drame", "Historique", "Policier"],
+    createdBy: ["Steven Knight"],
+    actors: ["Cillian Murphy", "Natasha O'Keeffe", "Helen McCrory"],
+    episodeDurationInMinutes: 52,
+    numberOfSeasons: 6,
+    numberOfEpisods: 36,
+    ongoing: false,
+  },
+  "Shingeki no Kyojin": {
+    releaseYear: 2013,
+    country: "Japon",
+    availableOn: "Netflix",
+    styles: ["Aventure", "Action", "Animation"],
+    createdBy: ["Yasuko Kobayashi"],
+    actors: ["Yûki Kaji", "Yui Ishikawa", "Marina Inoue"],
+    episodeDurationInMinutes: 24,
+    numberOfSeasons: 4,
+    numberOfEpisods: 95,
+    ongoing: true,
+  },
+  Sherlock: {
+    releaseYear: 2010,
+    country: "Grande-Bretagne",
+    availableOn: "Amazon",
+    styles: ["Aventure", "Drame", "Policier"],
+    createdBy: ["Steven Moffat", "Mark Gatiss"],
+    actors: ["Benedict Cumberbatch", "Martin Freeman", "Amanda Abbington"],
+    episodeDurationInMinutes: 90,
+    numberOfSeasons: 4,
+    numberOfEpisods: 14,
+    ongoing: false,
+  },
+  "The Queen's Gambit": {
+    releaseYear: 2020,
+    country: "U.S.A.",
+    availableOn: "Netflix",
+    styles: ["Drame"],
+    createdBy: ["Scott Frank", "Allan Scott"],
+    actors: ["Anya Taylor-Joy", "William Horberg", "Harry Melling"],
+    episodeDurationInMinutes: 60,
+    numberOfSeasons: 1,
+    numberOfEpisods: 7,
+    ongoing: false,
+  },
+  "When They See Us": {
+    releaseYear: 2019,
+    country: "U.S.A.",
+    availableOn: "Netflix",
+    styles: ["Drame"],
+    createdBy: ["Ava DuVernay"],
+    actors: ["Asante Blackk", "Caleel Harris", "Ethan Herisse"],
+    episodeDurationInMinutes: 35,
+    numberOfSeasons: 1,
+    numberOfEpisods: 4,
+    ongoing: false,
+  },
+  "Stranger Things": {
+    releaseYear: 2016,
+    country: "U.S.A.",
+    availableOn: "Netflix",
+    styles: ["Drame", "Fantastique", "Thriller"],
+    createdBy: ["Matt Duffer, Ross Duffer"],
+    actors: ["Winona Ryder", "Millie Bobby Brown", "Finn Wolfhard"],
+    episodeDurationInMinutes: 55,
+    numberOfSeasons: 5,
+    numberOfEpisods: 43,
+    ongoing: true,
+  },
+  "Hunter X Hunter": {
+    releaseYear: 2011,
+    country: "Japon",
+    availableOn: "Netflix",
+    styles: ["Aventure", "Comédie", "Animation"],
+    createdBy: ["Megumi Han", "Keiji Fujiwara", "Miyuki Sawashiro"],
+    actors: ["Megumi Han", "Keiji Fujiwara", "Miyuki Sawashiro", "Mariya Ise"],
+    episodeDurationInMinutes: 22,
+    numberOfSeasons: 4,
+    numberOfEpisods: 148,
+    ongoing: false,
+  },
+  Heartstopper: {
+    releaseYear: 2022,
+    country: "Grande-Bretagne",
+    availableOn: "Netflix",
+    styles: ["Drame", "Romance"],
+    createdBy: ["Alice Oseman"],
+    actors: ["Kit Connor", "Joe Locke", "Corinna Brown"],
+    episodeDurationInMinutes: 30,
+    numberOfSeasons: 3,
+    numberOfEpisods: 16,
+    ongoing: true,
+  },
+  Vikings: {
+    releaseYear: 2013,
+    country: "Canada",
+    availableOn: "Netflix",
+    styles: ["Aventure", "Drame", "Historique"],
+    createdBy: ["Michael Hirst"],
+    actors: ["Alex Høgh Andersen", "Gustaf Skarsgård", "Marco Ilsø"],
+    episodeDurationInMinutes: 52,
+    numberOfSeasons: 6,
+    numberOfEpisods: 89,
+    ongoing: false,
+  },
+  "True Detective": {
+    releaseYear: 2014,
+    country: "U.S.A.",
+    availableOn: "OCS",
+    styles: ["Drame", "Policier", "Thriller"],
+    createdBy: ["Nic Pizzolatto"],
+    actors: ["Mahershala Ali", "Stephen Dorff", "Carmen Ejogo"],
+    episodeDurationInMinutes: 55,
+    numberOfSeasons: 4,
+    numberOfEpisods: 24,
+    ongoing: true,
+  },
+  Shameless: {
+    releaseYear: 2011,
+    country: "U.S.A.",
+    availableOn: "Amazon",
+    styles: ["Comédie", "Drame"],
+    createdBy: ["Paul Abbott"],
+    actors: ["William H. Macy", "Jeremy Allen White", "Shanola Hampton"],
+    episodeDurationInMinutes: 45,
+    numberOfSeasons: 11,
+    numberOfEpisods: 134,
+    ongoing: false,
+  },
 };
-
 
 /* --------------------------------------------- */
 
@@ -190,27 +189,39 @@ console.log(series.Sherlock.country);
 
 /* --------------------------------------------- */
 
-console.info("2/ Quelle est la série la plus ancienne entre Breaking Bad et Shameless ?");
+console.info(
+  "2/ Quelle est la série la plus ancienne entre Breaking Bad et Shameless ?"
+);
 
 let older = "";
-if (series["Breaking Bad"].releaseYear < series.Shameless.releaseYear){
-    older="Bbreaking Bad";
+if (series["Breaking Bad"].releaseYear < series.Shameless.releaseYear) {
+  older = "Bbreaking Bad";
+} else {
+  older = "Shameless";
 }
-
-else {
-    older = "Shameless";}
 console.log(older);
 
-older = series["Breaking Bad"].releaseYear < series.Shameless.releaseYear ? "Bbreaking Bad" : "Shameless";
+older =
+  series["Breaking Bad"].releaseYear < series.Shameless.releaseYear
+    ? "Bbreaking Bad"
+    : "Shameless";
 console.log(older);
 
 /* --------------------------------------------- */
 
-console.info("3/ Quelle est la durée totale de la série Game of Thrones en heures et minutes ?");
+console.info(
+  "3/ Quelle est la durée totale de la série Game of Thrones en heures et minutes ?"
+);
 
-let durationGOT = series["Game of Thrones"].episodeDurationInMinutes * series["Game of Thrones"].numberOfEpisods;
+let durationGOT =
+  series["Game of Thrones"].episodeDurationInMinutes *
+  series["Game of Thrones"].numberOfEpisods;
 
-console.log(`le film a une durée de ${Math.floor(durationGOT/60)} heures et ${(durationGOT) % 60} minutes`);
+console.log(
+  `le film a une durée de ${Math.floor(durationGOT / 60)} heures et ${
+    durationGOT % 60
+  } minutes`
+);
 
 /* --------------------------------------------- */
 
@@ -228,36 +239,69 @@ console.info("4/ Donnez la liste des séries d'animation.");
 let animation = [];
 
 for (const title in series) {
-if (series[title].styles.includes("Animation")){
+  if (series[title].styles.includes("Animation")) {
     animation.push(title);
-            }
+  }
 }
 console.log(animation);
 /* --------------------------------------------- */
 
-console.info("5/ Il est 23h. J'ai décidé de faire une nuit blanche et de regarder une série complète avant 8h demain matin. quelles sont les séries complètes dont la durée est égale 8h-23h (9h) ");
+console.info(
+  "5/ Il est 23h. J'ai décidé de faire une nuit blanche et de regarder une série complète avant 8h demain matin. quelles sont les séries complètes dont la durée est égale 8h-23h (9h) "
+);
 console.info("Quelles sont les séries que je peux regarder ?");
+let films9h = [];
+let durationSeries;
+for (const title in series) {
+  if (
+    series[title].episodeDurationInMinutes * series[title].numberOfEpisods <
+    540
+  ) {
+    durationSeries = title;
+    films9h.push(durationSeries);
+  }
+}
+console.log(films9h);
+/* --------------------------------------------- */
 
+console.info(
+  "6/ Quelles sont les séries terminées originaires de Grande-Bretagne ou du Japon ?"
+);
+let filmArray = [];
+let strangersFilms = "";
+for (let c in series) {
+  if (
+    series[c].country === undefined ||
+    (series[c].country === "grande-Bretagne" && series[c].ongoing === "fase")
+  )
+    strangersFilms = c;
+  array.push(strangersFilms);
 
+  console.log(strangersFilms);
+  console.log(filmArray);
+}
+//   if (series[c].ongoing === false) {
+//     filmsfalse = c;
+//   }
+//   for (let filmsfalse in series) {
+
+//   }
+// }    array.push(strangersFilms)
+
+// console.log(array);
+/* --------------------------------------------- */
+
+console.info(
+  "7/ J'ai uniquement un abonnement Netflix. Quelles series de science fiction ou thriller puis-je regarder ?"
+);
 
 console.log();
 
 /* --------------------------------------------- */
 
-console.info("6/ Quelles sont les séries terminées originaires de Grande-Bretagne ou du Japon ?");
-
-console.log();
-
-/* --------------------------------------------- */
-
-console.info("7/ J'ai uniquement un abonnement Netflix. Quelles series de science fiction ou thriller puis-je regarder ?");
-
-
-console.log();
-
-/* --------------------------------------------- */
-
-console.info("8/ Retournez la liste des series avec pour chacune le nombre de personnes dans l'équipe de réalisation (createdBy) ?");
+console.info(
+  "8/ Retournez la liste des series avec pour chacune le nombre de personnes dans l'équipe de réalisation (createdBy) ?"
+);
 
 console.log();
 
@@ -269,18 +313,24 @@ console.log();
 
 /* --------------------------------------------- */
 
-console.info("10/ Quel est le style de série le plus représenté dans cette liste ?");
+console.info(
+  "10/ Quel est le style de série le plus représenté dans cette liste ?"
+);
 
 console.log();
 
 /* --------------------------------------------- */
 
-console.info("BONUS 1/ Retournez la liste des titres de toutes les séries classées de la plus récente à la plus ancienne.");
+console.info(
+  "BONUS 1/ Retournez la liste des titres de toutes les séries classées de la plus récente à la plus ancienne."
+);
 
 console.log();
 
 /* --------------------------------------------- */
 
-console.info("BONUS 2/ Je vais passer mes 8 jours de vacance à regarder les séries de cette liste dans l'ordre à raison de 16 heures par jour. Par quel épisode de quelle série vais-je conclure mes vacances ?");
+console.info(
+  "BONUS 2/ Je vais passer mes 8 jours de vacance à regarder les séries de cette liste dans l'ordre à raison de 16 heures par jour. Par quel épisode de quelle série vais-je conclure mes vacances ?"
+);
 
 console.log();
